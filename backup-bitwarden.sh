54250a671c17eab353c61fda2e564a620ce2c90d
NikolaInvernizzi <nikola.invernizzi@gmail.com>
2021-04-06 19:42:28 +0200

    update logging

--- backup-bitwarden.sh
+++ backup-bitwarden.sh
@@ -9,28 +9,39 @@ fi
 # create backup filename
 BACKUP_FILE="${BACKUP_PREFIX}_$(date "+%F-%H%M%S")"
 
+echo " " >> /logs/bitwarden_backup.log
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> /logs/bitwarden_backup.log
+echo "Starting backup process..." >> /logs/bitwarden_backup.log
+
 # use sqlite3 to create backup (avoids corruption if db write in progress)
 sqlite3 /data/db.sqlite3 ".backup '/tmp/db.sqlite3'"
 
 # tar up backup and encrypt with openssl and encryption key
-tar -czf - /tmp/db.sqlite3 /data/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
-
 echo "Creating backup: ${BACKUP_FILE}.tar.gz" >> /logs/bitwarden_backup.log
+tar -czf - /tmp/db.sqlite3 /data/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
+echo "Created backup." >> /logs/bitwarden_backup.log
+
 
 # upload encrypted tar to dropbox
+echo "Uploading backup to Dropbox." >> /logs/bitwarden_backup.log
 /dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE}.tar.gz /${BACKUP_FILE}.tar.gz
-
-echo "Uploaded backup: ${BACKUP_FILE}.tar.gz" >> /logs/bitwarden_backup.log
+echo "Uploaded backup to Dropbox." >> /logs/bitwarden_backup.log
 
 # copy to backups folder if env is given
 if [ ! -z "$KEEP_LOCAL_BACKUPS" ] && [ $KEEP_LOCAL_BACKUPS -eq 1 ]
 then
-  echo "Saved backup locally" >> /logs/bitwarden_backup.log
+  echo "Saving backup locally." >> /logs/bitwarden_backup.log
   cp /tmp/${BACKUP_FILE}.tar.gz /backups/${BACKUP_FILE}.tar.gz
+else
+  echo "Saving backup locally >> disabled <<" >> /logs/bitwarden_backup.log
 fi
 
 # cleanup tmp folder
 rm -rf /tmp/*
+
+echo " " >> /logs/bitwarden_backup.log
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >> /logs/bitwarden_backup.log
+echo " " >> /logs/bitwarden_backup.log
 
 # delete older backups if variable is set & greater than 0
 if [ ! -z $DELETE_AFTER ] && [ $DELETE_AFTER -gt 0 ]
