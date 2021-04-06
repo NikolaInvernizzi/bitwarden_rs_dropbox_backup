40a810bf2ae0e9c3dfec19d43d1c4c6280f7c709
NikolaInvernizzi <nikola.invernizzi@gmail.com>
2021-04-06 19:13:39 +0200

    changes setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@
 CONFIGFILE=/config/.dropbox_uploader
 if [ ! -f "$CONFIGFILE" ]; 
 then
-    echo "Setup failed, rerun /setup.sh"
-    echo "Setup failed, rerun /setup.sh" >> /logs/bitwarden_backup.log
+    echo "Setup failed, rerun /setup.sh."
+    echo "Setup failed, rerun /setup.sh." >> /logs/bitwarden_backup.log
     exit 0
 fi
 
-echo "Dropbox setup done" >> /logs/bitwarden_backup.log
+echo "Dropbox setup done!" >> /logs/bitwarden_backup.log
 /dropbox_uploader.sh -f /config/.dropbox_uploader info >> /logs/bitwarden_backup.log
 
 /backup-bitwarden.sh
